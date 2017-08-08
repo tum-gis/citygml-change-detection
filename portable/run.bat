c76003dfba5a6c6ce183e4df7167c43a2ffe9fad
n9s2cya <n9s2cya@hotmail.com>
2017-08-08 11:24:32 +0200

    Updated batch file.

--- portable/run.bat
+++ portable/run.bat
@@ -3,3 +3,5 @@ echo Deleting existing Neo4j Database...
 if exist neo4jDB rmdir /Q /S neo4jDB
 java -Xms1000m -Xmx1000m -XX:+UseG1GC -jar CityGMLChangeDetection.jar -SETTINGS="Settings.txt"
 rmdir /S neo4jDB
+echo Execution done.
+pause
