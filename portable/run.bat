3f2566d5f1bf2f2ac97865a19b0e2088fc801feb
n9s2cya <n9s2cya@hotmail.com>
2017-11-08 09:08:18 +0100

    Updated citygml4j and neo4j. Added some tests and settings.

--- portable/run.bat
+++ portable/run.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Deleting existing Neo4j Database...
 if exist neo4jDB rmdir /Q /S neo4jDB
-java -Xms1000m -Xmx1000m -XX:+UseG1GC -jar CityGMLChangeDetection.jar -SETTINGS="Settings.txt"
+java -Xms1000m -Xmx1000m -XX:+UseG1GC -jar CityGMLChangeDetection.jar -SETTINGS="settings/Default.txt"
 rmdir /S neo4jDB
 echo Execution done.
 pause
