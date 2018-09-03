47426753d3dc82a733e21e87488d01df3139cd3a
Son-HNguyen <son.nguyen@tum.de>
2018-09-03 16:41:58 +0200

    Add an option APPEARANCE_LOCATION in settings

--- portable/copyToServer.bat
+++ portable/copyToServer.bat
@@ -2,6 +2,6 @@
 set PATH=C:\Program Files\PuTTY
 set myvar=%cd%
 echo Please make sure the folder CityGMLChangeDetection already exists in the server.
-pscp -r %myvar% nguyen@ServerIP:/home/nguyen/RAMDISK/CityGMLChangeDetection
+pscp -r %myvar% nguyen@10.162.246.20:/home/nguyen/RAMDISK/CityGMLChangeDetection
 echo Data transfer to server finished.
 pause
