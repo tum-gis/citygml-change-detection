8c12a40c6b6b7e2ecdf57adc29ef96e189e68bd1
Son-HNguyen <son.nguyen@tum.de>
2018-09-06 12:54:02 +0200

    Clean codes

--- src/main/java/util/ClientUtil.java
+++ src/main/java/util/ClientUtil.java
@@ -69,16 +69,4 @@ public class ClientUtil {
 			System.out.println(logging.toString());
 		}
 	}
-
-	public static void main(String[] args) throws ClientProtocolException, IOException {
-		String url = "http://10.162.246.159:8080/citydb-wfs/wfs";
-		StringBuilder xmlContent = new StringBuilder();
-		xmlContent.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?> "
-				+ "<wfs:GetFeature service=\"WFS\" version=\"2.0.0\" "
-				+ "xmlns:wfs=\"http://www.opengis.net/wfs/2.0\" outputFormat=\"GML3.1+GZIP\"> "
-				+ "<wfs:StoredQuery id=\"http://www.opengis.net/def/query/OGC-WFS/0/GetFeatureById\"> "
-				+ "<wfs:Parameter name=\"id\">Test_Building</wfs:Parameter> </wfs:StoredQuery> </wfs:GetFeature>");
-
-		sendHttpPost(url, xmlContent, null);
-	}
 }
