bfadd81c31a55829922001de09654cc529382dc2
Son-HNguyen <son.nguyen@tum.de>
2018-09-06 12:06:38 +0200

    Clean files

--- portable/copyToServer.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set PATH=C:\Program Files\PuTTY
-set myvar=%cd%
-echo Please make sure the folder CityGMLChangeDetection already exists in the server.
-pscp -r %myvar% nguyen@10.162.246.20:/home/nguyen/RAMDISK/CityGMLChangeDetection
-echo Data transfer to server finished.
-pause
