4c3c9944a85be4a089bd28435043392056c3c6be
Son H. Nguyen <son.nguyen@tum.de>
2022-06-24 20:11:37 +0200

    Update RelationshipFactory

--- src/main/java/components/mapper/RelationshipFactory.java
+++ src/main/java/components/mapper/RelationshipFactory.java
@@ -5,5 +5,12 @@ import org.neo4j.graphdb.RelationshipType;
 public enum RelationshipFactory implements RelationshipType {
     OLD_CITY_MODEL,
     NEW_CITY_MODEL,
-    HREF
+    HREF;
+
+    public static RelationshipFactory getCityModelRel(boolean isOld) {
+        if (isOld) {
+            return OLD_CITY_MODEL;
+        }
+        return NEW_CITY_MODEL;
+    }
 }
