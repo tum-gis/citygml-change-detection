34c7da8c105322bfdc45cf28074cd297d84be00d
Son H. Nguyen <son.nguyen@tum.de>
2022-08-14 17:54:26 +0200

    Update enum class for printable types

--- src/main/java/components/mapper/PrintableClass.java
+++ src/main/java/components/mapper/PrintableClass.java
@@ -2,6 +2,8 @@ package components.mapper;
 
 import org.citygml4j.model.common.base.ModelClassEnum;
 
+import java.time.ZonedDateTime;
+
 public enum PrintableClass implements ModelClassEnum {
     UNDEFINED(null),
     BOOLEAN(Boolean.class),
@@ -12,7 +14,9 @@ public enum PrintableClass implements ModelClassEnum {
     LONG(Long.class),
     FLOAT(Float.class),
     DOUBLE(Double.class),
-    STRING(String.class);
+    STRING(String.class),
+    ZONED_DATE_TIME(ZonedDateTime.class);
+    // TODO More simple classes?
 
     private final Class<?> modelClass;
 
@@ -35,6 +39,22 @@ public enum PrintableClass implements ModelClassEnum {
         }
 
         return UNDEFINED;
+    }
+
+    /**
+     * Determines whether a given object is of a simple type, meaning their contents can be easily displayed as texts.
+     *
+     * @param object
+     * @return {@code true} if the object is of a simple type; {@code false} otherwise.
+     */
+    public static boolean isPrintable(Object object) {
+        Class cl = object.getClass();
+        for (PrintableClass p : values()) {
+            if (p.modelClass == cl) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Class<?> getModelClass() {
